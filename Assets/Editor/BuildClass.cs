37b3dca9f205d939de4472872d88121082d30b40
TAKATOU-PC\hisat <hisato.t05241123@gmail.com>
2018-01-23 10:16:52 +0900

    開始

---
+++ Assets/Editor/BuildClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BuildClass : MonoBehaviour
+{
+    public static void Build()
+    {
+        //ビルド対象シーンリスト
+        string[] sceneList =
+        {
+            "./Assets/Main.unity"
+        };
+
+        //実行
+        string errorMessage = BuildPipeline.BuildPlayer
+            (
+            sceneList,
+            "C:/cmdTest_for_sample_cp/BuildTest.apk",
+            BuildTarget.Android,
+            BuildOptions.Development
+            );
+
+        //出力結果
+        if (!string.IsNullOrEmpty(errorMessage))
+            Debug.LogError("[Error!]" + errorMessage);
+        else
+            Debug.Log("[Sucess!]");
+    }
+
+}
---
+++ Assets/Script/Rolling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling : MonoBehaviour {
+
+    public int x;
+    public int y;
+    public int z;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(x, y, z));
+    }
+}
